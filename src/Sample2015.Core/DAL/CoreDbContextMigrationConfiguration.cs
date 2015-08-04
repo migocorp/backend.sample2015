3cd634dc899dc5184acbb8b38e621247c2ab7f84
Jim-Lin <jim_lin@migocorp.com>
2015-08-04 16:45:33 +0800

    create CoreDbContextMigrationConfiguration

---
+++ src/Sample2015.Core/DAL/CoreDbContextMigrationConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample2015.Core.DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal sealed class CoreDbContextMigrationConfiguration : DbMigrationsConfiguration<CoreDbContext>
+    {
+        public CoreDbContextMigrationConfiguration()
+        {
+            //// Use command:  Update-Database -Script -StartUpProjectName "Sample2015.Core"
+
+            //// if need to drop table:
+            //// this.AutomaticMigrationDataLossAllowed = true;
+
+            this.AutomaticMigrationsEnabled = true;
+            this.ContextKey = "Sample2015.Core.DAL.CoreDbContext";
+        }
+    }
+}
